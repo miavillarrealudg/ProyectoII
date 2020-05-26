5af7fe60c2e07eb942f72058cdbbcaf3b0558be1
mia <mia.villarreal@udgvirtual.udg.mx>
2020-05-25 20:13:39 -0500

    Unidad1ActIntegradora

---
+++ src/components/Navbars/AdminNavbarLinks.jsx
@@ -0,0 +1,77 @@
+/*!
+
+=========================================================
+* Light Bootstrap Dashboard React - v1.3.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/light-bootstrap-dashboard-react
+* Copyright 2019 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/light-bootstrap-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React, { Component } from "react";
+import { NavItem, Nav, NavDropdown, MenuItem } from "react-bootstrap";
+
+class AdminNavbarLinks extends Component {
+  render() {
+    const notification = (
+      <div>
+        <i className="fa fa-globe" />
+        <b className="caret" />
+        <span className="notification">5</span>
+        <p className="hidden-lg hidden-md">Notification</p>
+      </div>
+    );
+    return (
+      <div>
+        {/* <Nav>
+          <NavItem eventKey={1} href="#">
+            <i className="fa fa-dashboard" />
+            <p className="hidden-lg hidden-md">Dashboard</p>
+          </NavItem>
+          <NavDropdown
+            eventKey={2}
+            title={notification}
+            noCaret
+            id="basic-nav-dropdown"
+          >
+            <MenuItem eventKey={2.1}>Notification 1</MenuItem>
+            <MenuItem eventKey={2.2}>Notification 2</MenuItem>
+            <MenuItem eventKey={2.3}>Notification 3</MenuItem>
+            <MenuItem eventKey={2.4}>Notification 4</MenuItem>
+            <MenuItem eventKey={2.5}>Another notifications</MenuItem>
+          </NavDropdown>
+          <NavItem eventKey={3} href="#">
+            <i className="fa fa-search" />
+            <p className="hidden-lg hidden-md">Search</p>
+          </NavItem>
+        </Nav> */}
+        
+        
+
+        <Nav pullRight>
+        <NavItem eventKey={3} href="#">
+            <i className="fa fa-search" />
+            <p className="hidden-lg hidden-md">Search</p>
+          </NavItem>
+
+          <NavItem eventKey={3} href="#">
+            <p>Reestablecer</p>
+          </NavItem>
+           
+          <NavItem eventKey={3} href="#">
+            Cerrar sesión
+          </NavItem>
+        </Nav>
+      </div>
+    );
+  }
+}
+
+export default AdminNavbarLinks;
---
+++ src/views/Pagos.jsx
@@ -0,0 +1,45 @@
+/*!
+
+=========================================================
+* Light Bootstrap Dashboard React - v1.3.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/light-bootstrap-dashboard-react
+* Copyright 2019 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/light-bootstrap-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React, { Component } from "react";
+import {
+    Grid,
+    Row,
+    Col,
+    FormGroup,
+    ControlLabel,
+    FormControl
+} from "react-bootstrap";
+
+import { Card } from "components/Card/Card.jsx";
+import { FormInputs } from "components/FormInputs/FormInputs.jsx";
+import { UserCard } from "components/UserCard/UserCard.jsx";
+import Button from "components/CustomButton/CustomButton.jsx";
+
+import avatar from "assets/img/faces/face-3.jpg";
+
+class Pagos extends Component {
+    render() {
+        return (
+            <div className="content">
+
+            </div>
+        );
+    }
+}
+
+export default Pagos;
